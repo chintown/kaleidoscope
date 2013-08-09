d9256dfc4e02551b79d157adc124d11b9edebdaa
Mike Chen <chintown@gmail.com>
2013-08-09 21:43:21 +0800

    add a new tab, look ahead

---
+++ kaleidoscope/KSHeadlineViewController.h
@@ -0,0 +1,13 @@
+//
+//  KSHeadlineViewController.h
+//  kaleidoscope
+//
+//  Created by Mike Chen on 8/9/13.
+//  Copyright (c) 2013 Mike Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSHeadlineViewController : UITableViewController
+
+@end
