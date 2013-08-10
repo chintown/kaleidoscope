13f7c2414857a5513d3fde7c26cac3cf594caa3b
Mike Chen <chintown@gmail.com>
2013-08-10 15:53:13 +0800

    add customize cell for headline

---
+++ kaleidoscope/KSHeadlineCell.h
@@ -0,0 +1,14 @@
+//
+//  KSHeadlineCell.h
+//  kaleidoscope
+//
+//  Created by Mike Chen on 8/10/13.
+//  Copyright (c) 2013 Mike Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSHeadlineCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UITextView *subText;
+@property (weak, nonatomic) IBOutlet UITextView *headline;
+@end
---
+++ kaleidoscope/KSHeadlineCell.m
@@ -0,0 +1,29 @@
+//
+//  KSHeadlineCell.m
+//  kaleidoscope
+//
+//  Created by Mike Chen on 8/10/13.
+//  Copyright (c) 2013 Mike Chen. All rights reserved.
+//
+
+#import "KSHeadlineCell.h"
+
+@implementation KSHeadlineCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
